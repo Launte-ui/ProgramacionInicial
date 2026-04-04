e85962f853885f3303b8e7a383990500a9f57020
Lautaro <Lautaro.ponti.2005@gmail.com>
2026-04-04 20:58:16 -0300

    test printf

--- Programacion Estructurada Basica/NumerosAleatorios.c
+++ Programacion Estructurada Basica/NumerosAleatorios.c
@@ -1,13 +1,25 @@
 #include <stdio.h>
 #include <time.h>
+#define TAM 1
+
+void CargaAleatoria(int [], int, int);
+void Mostrar(int [], int);
 
 int main(){
-    int num;
-    srand(time(NULL)); // Inicializa la semilla para generar números aleatorios
-
-    num = rand(); // Genera un número aleatorio entre 0 y 99
-    printf("Numero aleatorio: %d\n", num);
+    int vec[TAM];
+    srand(time(NULL));
+    CargaAleatoria(vec, TAM, 6); // Carga el vector con números aleatorios entre 0 y 99
+    Mostrar(vec, TAM); // Muestra los números aleatorios generados
 
     return 0;
+}
 
+void CargaAleatoria(int v[], int ce, int max){
+    for(int i = 0; i < ce; i++)    
+        v[i] = rand() % max;
+}
+
+void Mostrar(int v[], int ce){
+    for(int i = 0; i < ce; i++)    
+        printf("%d\n", v[i]);
 }
--- Programacion Estructurada Basica/Unidad1/Ejercicio1.1.c
+++ Programacion Estructurada Basica/Unidad1/Ejercicio1.1.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <time.h>
-#define TAM 10
+#define TAM 20
 
 void CargaAleatoria(int [], int, int);
 void Mostrar(int [], int);
+
 int main(){
     int vec[TAM];
     srand(time(NULL));
-    CargaAleatoria(vec, TAM, 6); // Carga el vector con números aleatorios entre 0 y 99
+    CargaAleatoria(vec, TAM, 1000); // Carga el vector con números aleatorios entre 0 y 99
     Mostrar(vec, TAM); // Muestra los números aleatorios generados
 
     return 0;
